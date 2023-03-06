13ed7bc64258e4868c2a4c365626fae84fac44b7
sun-unc <884226421@qq.com>
2023-03-06 09:45:19 +0800

    更新手写函数

--- JavaScript/手写call、apply、bind.js
+++ JavaScript/手写call、apply、bind.js
@@ -1,29 +1,47 @@
 // call
 function call(target, ...args) {
+  // 如果不是函数调用 myCall()，则抛出TypeError
+  if (typeof this !== "function") {
+    throw new Error(`${this} is not a function`);
+  }
+  // 如果未提供target，则使用全局对象window
   target = target || window;
+  // 将target转换为对应的包装对象，以便使用target调用函数
+  target = Object(target);
+  // 通过Symbol()创建一个唯一的属性名，以避免覆盖对象的现有属性
   const symbolKey = Symbol();
   target[symbolKey] = this;
-  const res = target[symbolKey](...args);
+  const result = target[symbolKey](...args);
   delete target[symbolKey];
-  return res;
+  return result;
 }
+
 //apply
 function apply(target, args) {
+  if (typeof this !== "function") {
+    throw new Error(`${this} is not a function`);
+  }
   target = target || window;
+  target = Object(target);
   const symbolKey = Symbol();
   target[symbolKey] = this;
   const res = target[symbolKey](...args);
   delete target[symbolKey];
   return res;
 }
+
 // bind
 function bind(target, ...args) {
-  target = target || window;
-  const symbolKey = Symbol();
-  target[symbolKey] = this;
-  return function (...innerArgs) {
-    const res = target[symbolKey](...args, ...innerArgs);
-    // delete target[symbolKey] 这里不能销毁绑定的函数，否则第二次调用的时候，就会出现问题。
-    return res;
+  if (typeof this !== "function") {
+    throw new Error(`${this} is not a function`);
+  }
+  // 保存this的引用
+  const self = this;
+  return function (...secArgs) {
+    // bind返回的函数是作为构造函数时,this指向实例化对象
+    if (this instanceof self) {
+      return new self(...args, ...secArgs);
+    }
+    return self.apply(target, [...args, ...secArgs]);
   };
 }
--- JavaScript/手写new操作符.js
+++ JavaScript/手写new操作符.js
@@ -1,9 +1,16 @@
+/**
+ * 1、创建一个新的空对象，该对象是要实例化的类的实例。
+ * 2、将该对象的原型指向要实例化的类的原型，以便该对象可以访问该类的原型中定义的方法和属性。
+ * 3、将该对象作为 this 参数调用要实例化的类的构造函数，以便将属性和方法添加到该对象上。
+ * 4、如果构造函数返回一个对象，则返回该对象；否则，返回创建的新对象。
+ */
+
 // 方式一
 function mynew(Func, ...args) {
-  // 创建一个新对象
-  const obj = {};
-  // 新对象的隐式原型指向构造函数的显示原型
-  obj.__proto__ = Func.Prototype;
+  // 创建一个新对象，并将新对象的隐式原型指向构造函数的显示原型
+  const obj = Object.create(Func.prototype);
+  // const obj = {};
+  // Object.setPrototypeOf(obj, Func.prototype); //这种方式更改原型也是可以的
   // 将构造函数的this指向新对象
   const result = Func.apply(obj, args);
   return result instanceof Object ? result : obj;
@@ -12,7 +19,7 @@ function mynew(Func, ...args) {
 // 方式二
 function create() {
   const Func = [].shift.call(arguments);
-  const obj = Object.create(Func);
+  const obj = Object.create(Func.prototype);
   const result = Func.apply(obj, arguments);
   return result instanceof Object ? result : obj;
 }
